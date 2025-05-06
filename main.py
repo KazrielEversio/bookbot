e63260d182cbce4aea261e70e4c16ffe2c37565a
KazrielEversio <VarlenVentru@Yahoo.com>
2025-05-06 19:05:13 -0400

    Completed Bookbot

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+import sys
 def getBookText(path):
     with open(path) as f:
         bookText = f.read()
     return (bookText)
 
 from stats import wordCount
+from stats import letters
 
 def main():
-    x = getBookText("./books/frankenstein.txt")
-    wordCount(x)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        x = getBookText(sys.argv[1])
+        wordCount(x)
+        letters(x)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,17 @@
 def wordCount(bookText):
     num_words = len(bookText.split())
-    print(num_words, "words found in the document")
+    print("Found", num_words, "total words")
+    return(bookText)
+
+def letters(bookText):
+    letters = bookText.lower()
+    lettersDict = {}
+    for char in letters:
+        if char.isalpha():
+            lettersDict[char] = lettersDict.get(char, 0) + 1
+    items = list(lettersDict.items())
+    items.sort(key=lambda x: x[1], reverse=True)
+    sortedLetters = dict(items)
+
+    for char, count in sortedLetters.items():
+        print(f"{char}: {count}")
